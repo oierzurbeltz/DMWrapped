21f9a6608ebac3ed0ff55a45ded44a453116039b
oierzurbeltz <oieramelibia@gmail.com>
2023-11-22 18:06:22 +0100

    karpeta sartu

--- DMWrapped.py
+++ DMWrapped.py
@@ -3,6 +3,13 @@ from weka.core.converters import Loader
 from weka.classifiers import Classifier, Evaluation
 from weka.filters import Filter
 from weka.core.classes import Random
+
 jwm.start()
-## Hemen kodigoa
+def data_loader(convert, filter):
+    data_dir = "/users/1059715/Desktop/DMWrapped/DMDatuak/" + convert
+    loader = Loader(classname="weka.core.converters.ArffLoader")
+    data = loader.load_file(data_dir + filter + "_" + convert + ".arff")
+    data.class_is_last()
+
+
 jwm.stop()
