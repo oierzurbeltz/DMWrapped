ef41090e695054c1922cd4799273b2630df40a98
oierzurbeltz <oieramelibia@gmail.com>
2023-11-22 17:48:07 +0100

    berria

--- DMWrapped.py
+++ DMWrapped.py
@@ -1,1 +1,8 @@
- 
+import weka.core.jvm as jwm
+from weka.core.converters import Loader
+from weka.classifiers import Classifier, Evaluation
+from weka.filters import Filter
+from weka.core.classes import Random
+jwm.start()
+## Hemen kodigoa
+jwm.stop()
