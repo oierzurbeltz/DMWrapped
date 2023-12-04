5b0686c6d3af43d43f999befa9f514ca97ef4fcd
Binsi21 <benatc10@gmail.com>
2023-12-04 18:20:31 +0100

    labo

--- DMWrapped.py
+++ DMWrapped.py
@@ -6,9 +6,11 @@ from weka.core.classes import Random
 import weka.plot.graph as graph
 
 
-jwm.start()
-convert=input("Aukeratu convert-a: ")
-filter= input("Aukeratu filterr-a: ")
+jwm.start(packages=True)
+#convert=input("Aukeratu convert-a: ")
+#filter= input("Aukeratu filterr-a: ")
+convert="Edge"
+filter="PHOGFilter"
 def data_loader(convert, filter):
     data_dir = "./DMDatuak/" + convert + "/"
     loader = Loader(classname="weka.core.converters.ArffLoader")
@@ -20,11 +22,18 @@ def data_loader(convert, filter):
 data = data_loader(convert, filter)
 print(data)
 
-cls = Classifier(classname="weka.classifiers.trees.J48", options=["-C", "0.3"])
-cls.build_classifier(data)
+remove = Filter(classname="weka.filters.unsupervised.attribute.Remove", options=["-R", "first"])
+remove.inputformat(data)
+filtered_data = remove.filter(data)
 
-print(cls)
-graph.plot_dot_graph(cls.graph)
+data.class_is_last()	
+classifier = Classifier(classname="weka.classifiers.functions.SMO", options=["-C", "1.0", "-L", "0.001", "-P", "1.0E-12", "-N", "0", "-V", "-1", "-W", "1"])
+evaluation = Evaluation (filtered_data)
+evaluation.crossvalidate_model (classifier, filtered_data, 10, Random(42)) 
+print (evaluation.summary())
+print ("pctCorrect: "+ str(evaluation.percent_correct))
+print ("incorrect: " + str (evaluation.incorrect))
 
+#classifier = Classifier(classname="weka.filters.supervised.attribute.AttributeSelection -S "weka.attributeSelection.Ranker -N 50"  -E "weka.attributeSelection.ReliefFAttributeEval -M -1 -D 1 -K 10" -i $1.arff -o $1FSS.arff -r $2.arff -s $2FSS.arff -c last -b")
 
 jwm.stop()
